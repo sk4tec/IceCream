05d606242e8bcc54b458168116fa1de54947ed3d
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-06-20 19:20:01 +0100

    Initial Commit

---
+++ IceCream/IceCreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceCreamApp.swift
+//  IceCream
+//
+//  Created by Sunjay Kalsi on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct IceCreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
